6827fa0376e6093466a88e422d1b598cf339e882
ulziko <ulziihishig247@gmail.com>
2023-12-08 10:19:38 +0800

    header implemented

--- main.cpp
+++ main.cpp
@@ -1,60 +1,65 @@
-<<<<<<< HEAD
+
 int main()
 {
-=======
-const size_t STACK_SIZE=100;
-template<class T>
-class TStack{
-    protected:
-        int numItems;
-        T *items;
-    public:
-    TStack(size_t size=STACK_SIZE){
-        // printf("\nsize: %d",size);
-        numItems=-1;
-        items=new T[size];
+  const size_t STACK_SIZE = 100;
+  template <class T>
+  class TStack
+  {
+  protected:
+    int numItems;
+    T *items;
+
+  public:
+    TStack(size_t size = STACK_SIZE)
+    {
+      // printf("\nsize: %d",size);
+      numItems = -1;
+      items = new T[size];
     }
-    ~TStack(){delete[] items;}
+    ~TStack() { delete[] items; }
     void push(T t);
     T pop();
     int stackEmpty();
     int stackFull();
     void print();
-};
-template<class T>
-void TStack<T>::push(T t)
-{
-    if(!stackFull()) items[++numItems]=t;
-}
-template<class T>
-T TStack<T>::pop()
-{
-    if(!stackEmpty()) return items[numItems--];
+  };
+  template <class T>
+  void TStack<T>::push(T t)
+  {
+    if (!stackFull())
+      items[++numItems] = t;
+  }
+  template <class T>
+  T TStack<T>::pop()
+  {
+    if (!stackEmpty())
+      return items[numItems--];
     return 0;
-}
-template<class T>
-int TStack<T>::stackEmpty()
-{
-    return numItems==-1;
-}
-template<class T>
-int TStack<T>::stackFull()
-{
-    return numItems==STACK_SIZE;
-}
-template<class T>
-void TStack<T>::print()
-{
-    int i=numItems;
-    while(i>=0)
+  }
+  template <class T>
+  int TStack<T>::stackEmpty()
+  {
+    return numItems == -1;
+  }
+  template <class T>
+  int TStack<T>::stackFull()
+  {
+    return numItems == STACK_SIZE;
+  }
+  template <class T>
+  void TStack<T>::print()
+  {
+    int i = numItems;
+    while (i >= 0)
     {
-        cout<<items[i--]<<endl;
+      cout << items[i--] << endl;
     }
-}
+  }
 
-int main(){
->>>>>>> f45b702a7ecb170525e8c76e7ee332c747caadc3
-  printf("hello world!");
-  printf("hi ");
-  return 0;
+  int main()
+  {
+    printf("hello world!");
+    printf("hi ");
+    return 0;
+  }
 }
