83467e28613b72f8ea09394d3fafc694ba5bfd5c
Nana Batkhurel <119820080+Nanabn@users.noreply.github.com>
2023-12-10 11:45:03 +0800

    comment

--- main.cpp
+++ main.cpp
@@ -1,54 +1,136 @@
-const size_t STACK_SIZE=100;
-template<class T>
-class TStack{
-    protected:
-        int numItems;
-        T *items;
-    public:
-    TStack(size_t size=STACK_SIZE){
-        // printf("\nsize: %d",size);
-        numItems=-1;
-        items=new T[size];
-    }
-    ~TStack(){delete[] items;}
-    void push(T t);
-    T pop();
-    int stackEmpty();
-    int stackFull();
-    void print();
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class TwoDShape {
+public:
+    virtual void read() = 0;
+    virtual float area() const = 0;
+    virtual const char* getName() const = 0;
+    virtual ~TwoDShape() {}
 };
-template<class T>
-void TStack<T>::push(T t)
-{
-    if(!stackFull()) items[++numItems]=t;
-}
-template<class T>
-T TStack<T>::pop()
-{
-    if(!stackEmpty()) return items[numItems--];
-    return 0;
-}
-template<class T>
-int TStack<T>::stackEmpty()
-{
-    return numItems==-1;
-}
-template<class T>
-int TStack<T>::stackFull()
-{
-    return numItems==STACK_SIZE;
-}
-template<class T>
-void TStack<T>::print()
-{
-    int i=numItems;
-    while(i>=0)
-    {
-        cout<<items[i--]<<endl;
+
+class Circle : public TwoDShape {
+private:
+    float radius;
+
+public:
+    Circle() : radius(0.0) {}
+
+    void read() override {
+        cout << "Enter the radius of the circle: ";
+        cin >> radius;
     }
+
+    float area() const override {
+        return 3.14159 * radius * radius; // Assuming pi is approximately 3.14159
+    }
+
+    const char* getName() const override {
+        return "Circle";
+    }
+};
+
+class Square : public TwoDShape {
+private:
+    float sideLength;
+
+public:
+    Square() : sideLength(0.0) {}
+
+    void read() override {
+        cout << "Enter the side length of the square: ";
+        cin >> sideLength;
+    }
+
+    float area() const override {
+        return sideLength * sideLength;
+    }
+
+    const char* getName() const override {
+        return "Square";
+    }
+};
+
+class Triangle : public TwoDShape {
+private:
+    float base;
+    float height;
+
+public:
+    Triangle() : base(0.0), height(0.0) {}
+
+    void read() override {
+        cout << "Enter the base length of the triangle: ";
+        cin >> base;
+        cout << "Enter the height of the triangle: ";
+        cin >> height;
+    }
+
+    float area() const override {
+        return 0.5 * base * height;
+    }
+
+    const char* getName() const override {
+        return "Triangle";
+    }
+};
+
+float getAreaWrapper(TwoDShape* shape) {
+    return shape->area();
 }
 
-int main(){
-  printf("hiiiii!");
-  return 0;
+int main() {
+    const int numberOfShapes = 20;
+
+    // Seed the random number generator
+    srand(static_cast<unsigned>(time(0)));
+
+    vector<TwoDShape*> shapesList;
+
+    for (int i = 0; i < numberOfShapes; i++) {
+        char shapeType;
+        shapeType = 'A' + rand() % 3; // 'A' for Circle, 'B' for Square, 'C' for Triangle
+
+        TwoDShape* shape = nullptr;
+
+        switch (shapeType) {
+            case 'A':
+                shape = new Circle();
+                break;
+            case 'B':
+                shape = new Square();
+                break;
+            case 'C':
+                shape = new Triangle();
+                break;
+        }
+
+        shape->read();
+        shapesList.push_back(shape);
+    }
+
+    cout << "Shapes before sorting by area:\n";
+    for (const auto& shape : shapesList) {
+        cout << "Name: " << shape->getName() << endl;
+        cout << "Area: " << shape->area() << endl;
+        cout << "------------------" << endl;
+    }
+
+    // Sorting the shapes by area
+    sort(shapesList.begin(), shapesList.end(), [](TwoDShape* a, TwoDShape* b) {
+        return getAreaWrapper(a) < getAreaWrapper(b);
+    });
+
+    cout << "Shapes after sorting by area:\n";
+    for (const auto& shape : shapesList) {
+        cout << "Name: " << shape->getName() << endl;
+        cout << "Area: " << shape->area() << endl;
+        cout << "------------------" << endl;
+        delete shape; // Don't forget to delete dynamically allocated objects
+    }
+
+    return 0;
 }
